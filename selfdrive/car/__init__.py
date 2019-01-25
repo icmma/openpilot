97bd11d4559e98a0659df2994819665aa2efc342
Andrew Frahn <emmertex@gmail.com>
2019-01-25 20:35:31 +1100

    bug

--- selfdrive/car/__init__.py
+++ selfdrive/car/__init__.py
@@ -35,6 +35,8 @@ def limit_steer_rate(apply_torque, apply_torque_last, LIMITS):
     apply_torque = clip(apply_torque, apply_torque_last - LIMITS.STEER_DELTA_UP,
                                     min(apply_torque_last + LIMITS.STEER_DELTA_DOWN, LIMITS.STEER_DELTA_UP))
 
+  return int(round(apply_torque))
+
 def apply_toyota_steer_torque_limits(apply_torque, apply_torque_last, motor_torque, LIMITS):
 
   # limits due to comparison of commanded torque VS motor reported torque
