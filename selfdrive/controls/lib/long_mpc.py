14dd20839394ec26926805b190f196bcaab7182a
Shane <shane@smiskol.com>
2019-03-29 00:56:18 -0500

    Added dynamic follow to stock openpilot

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from scipy import interpolate
 import selfdrive.messaging as messaging
 from selfdrive.swaglog import cloudlog
 from common.realtime import sec_since_boot
@@ -21,8 +21,10 @@ class LongitudinalMpc(object):
     self.prev_lead_status = False
     self.prev_lead_x = 0.0
     self.new_lead = False
-
     self.last_cloudlog_t = 0.0
+
+    self.velocity_list = []
+    self.last_cost = 0
 
   def send_mpc_solution(self, qp_iterations, calculation_time):
     qp_iterations = max(0, qp_iterations)
@@ -55,7 +57,40 @@ class LongitudinalMpc(object):
     self.cur_state[0].v_ego = v
     self.cur_state[0].a_ego = a
 
-  def update(self, CS, lead, v_cruise_setpoint):
+  def get_acceleration(self):  # calculate car's own acceleration to generate more accurate following distances
+    a = (self.velocity_list[-1] - self.velocity_list[0])  # first half of acceleration formula
+    a = a / (len(
+      self.velocity_list) / 100.0)  # divide difference in velocity by how long in seconds the velocity list has been tracking velocity (2 sec)
+    if abs(a) < 0.11176:  # if abs(acceleration) is less than 0.25 mph/s, return 0
+      return 0.0
+    else:
+      return a
+
+  def generate_cost(self, distance):
+    x = [.9, 1.8, 2.7]
+    y = [1.0, .1, .05]
+
+    return round(float(np.interp(distance, x, y)), 2)
+
+  def generateTR(self, velocity, relative_velocity):  # in m/s
+    x = [0.0, 1.86267, 3.72533, 5.588, 7.45067, 9.31333, 11.55978, 13.645, 22.352, 31.2928, 33.528, 35.7632, 40.2336]  # velocity
+    y = [1.03, 1.05363, 1.07879, 1.11493, 1.16969, 1.25071, 1.36325, 1.43, 1.6, 1.7, 1.75618, 1.85, 2.0]  # distance
+
+    TR = interpolate.interp1d(x, y, fill_value='extrapolate')(velocity)[()]  # extrapolate above 90 mph
+
+    x = [-11.176, -7.84276, -4.67716, -2.12623, 0, 1.34112, 2.68224]  # relative velocity values, mph: [-25, -17.5, -10.5, -4.75, 0, 3, 6]
+    y = [(TR + .425), (TR + .320565), (TR + .257991), (TR + .126369), TR, (TR - .18), (TR - .3)]  # modification values
+
+    TR = np.interp(relative_velocity, x, y)  # interpolate as to not modify too much
+
+    x = [-4.4704, -2.2352, -0.89408, 0, 1.34112]  # acceleration values, mph: [-10, -5, -2, 0, 3]
+    y = [(TR + .395), (TR + .155), (TR + .0225), TR, (TR - .325)]  # modification values
+
+    TR = np.interp(self.get_acceleration(), x, y)
+
+    return round(TR, 2)
+
+  def update(self, CS, lead, v_cruise_setpoint, relative_velocity):
     v_ego = CS.carState.vEgo
 
     # Setup current mpc state
@@ -90,7 +125,20 @@ class LongitudinalMpc(object):
 
     # Calculate mpc
     t = sec_since_boot()
-    n_its = self.libmpc.run_mpc(self.cur_state, self.mpc_solution, self.a_lead_tau, a_lead)
+
+    # dynamic follow
+    if len(self.velocity_list) > 200 and len(self.velocity_list) != 0:  # 100hz, so 200 items is 2 seconds
+      self.velocity_list.pop(0)
+    self.velocity_list.append(v_ego)
+
+    TR = self.generateTR(v_ego, relative_velocity)
+    generated_cost = self.generate_cost(TR)
+
+    if abs(generated_cost - self.last_cost) > .15:
+      self.libmpc.init(MPC_COST_LONG.TTC, generated_cost, MPC_COST_LONG.ACCELERATION, MPC_COST_LONG.JERK)
+      self.last_cost = generated_cost
+
+    n_its = self.libmpc.run_mpc(self.cur_state, self.mpc_solution, self.a_lead_tau, a_lead, TR)
     duration = int((sec_since_boot() - t) * 1e9)
     self.send_mpc_solution(n_its, duration)
 
